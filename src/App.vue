49ef6c3ef1348fc8232e9848e496bdeac86354e8
Ivan Plastun <plastun.ivan2018@yandex.ru>
2021-04-09 15:08:59 +0300

    added filter and pagination for tickers

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
               >
               <div class="mt-1 relative rounded-md shadow-md">
                 <input
-                  v-model="ticker"
+                  v-model.trim="ticker"
                   @keydown.enter="add"
                   type="text"
                   name="wallet"
@@ -66,11 +66,18 @@
           </button>
         </section>
         <template v-if="tickers.length">
+          <div>
+            <button v-if="page > 1" @click="page -= 1" class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">Назад</button>
+            <button v-if="hasNextPage" @click="page += 1" class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">Вперед</button>
+            <div> Фильтр:
+              <input v-model.trim="filter" class="pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md" type="text">
+            </div>
+          </div>
           <hr class="w-full border-t border-gray-600 my-4" />
           <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
             <div
               @click="select(t)"
-              v-for="t in tickers"
+              v-for="t in filteredTickers()"
               :key="t.name"
               :class="{'border-4': sel === t}"
               class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
@@ -159,14 +166,41 @@ export default {
       ticker: null,
       tickers: [],
       sel: null,
-      graph: []
+      graph: [],
+
+      page: 1,
+      hasNextPage: false,
+      filter: ''
     }
   },
   created () {
     const tickersData = localStorage.getItem('criptonomicon-list')
-    this.tickers = JSON.parse(tickersData) ?? []
+    const windowData = Object.fromEntries(new URL(window.location).searchParams.entries())
 
-    // this.tickers.forEach(ticker => this.subscribeToUpdate(ticker.name))
+    console.log(windowData)
+
+    if (windowData.filter) {
+      this.filter = windowData.filter
+    }
+
+    if (windowData.page) {
+      this.page = +windowData.page
+    }
+
+    if (tickersData) {
+      this.tickers = JSON.parse(tickersData)
+      // this.tickers.forEach(ticker => this.subscribeToUpdate(ticker.name))
+    }
+
+  },
+  watch: {
+    filter () {
+      this.page = 1
+      window.history.pushState(null, document.title, `${window.location.pathname}?filter=${this.filter}&page=${this.page}`)
+    },
+    page () {
+      window.history.pushState(null, document.title, `${window.location.pathname}?filter=${this.filter}&page=${this.page}`)
+    }
   },
   methods: {
     add () {
@@ -179,6 +213,16 @@ export default {
       localStorage.setItem('criptonomicon-list', JSON.stringify(this.tickers))
       // this.subscribeToUpdate(currentTicker.name)
       this.ticker = ''
+      this.filter = ''
+    },
+    filteredTickers() {
+      const start = (this.page - 1) * 6
+      const end = this.page * 6
+      const filteredTickers = this.tickers.filter(ticker => ticker.name.includes(this.filter))
+
+      this.hasNextPage = filteredTickers.length > end
+
+      return filteredTickers.slice(start, end)
     },
     subscribeToUpdate(tickerName) {
       setInterval(async () => {
